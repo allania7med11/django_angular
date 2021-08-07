63ebf90d4396859526ffef1bea5228ada4e3233a
allania7med11 <allania7med11@gmail.com>
2021-08-07 17:57:19 +0100

    upload files

--- server/EmployeeApp/urls.py
+++ server/EmployeeApp/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     url(r'^department/$', views.departmentApi),
     url(r'^department/([0-9]+)$', views.departmentApi),
     url(r'^employee/$', views.employeeApi),
-    url(r'^employee/([0-9]+)$', views.employeeApi)
-]
+    url(r'^employee/([0-9]+)$', views.employeeApi),
+    url(r'^SaveFile$', views.SaveFile)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/EmployeeApp/views.py
+++ server/EmployeeApp/views.py
@@ -4,7 +4,7 @@ from rest_framework.parsers import JSONParser
 from EmployeeApp.models import Departments,Employees
 from EmployeeApp.serializers import DepartmentSerializer,EmployeeSerializer
 from django.http.response import JsonResponse
-
+from django.core.files.storage import default_storage
 # Create your views here.
 @csrf_exempt
 def departmentApi(request,id=0):
@@ -60,3 +60,10 @@ def employeeApi(request,id=0):
         employee=Employees.objects.get(EmployeeId=id)
         employee.delete()
         return JsonResponse("Deleted Succeffully!!", safe=False)
+
+@csrf_exempt
+def SaveFile(request):
+    file=request.FILES['uploadedFile']
+    file_name = default_storage.save(file.name,file)
+
+    return JsonResponse(file_name,safe=False)
